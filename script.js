f3e1e52190fc485ae1f9e50a63c1da46b2b9f475
victoryuen <dhuce3@gmail.com>
2024-01-23 16:47:20 -0500

    awdawd

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 let loginTracker = 0;
-
+let userName = "";
+let password = "";
 const openLogin = () =>{
   if(loginTracker == 0){
     loginContainer.style.display = "flex";
     loginContainer.style.zIndex = 1;
     loginTracker = 1;
-    document.querySelector("#lightbox").style.display = "block";
+    lightbox.style.display = "block";
   }
   else if (loginTracker == 1){
     loginContainer.style.display = "none";
     loginTracker = 0;
   }
   
-
+}
+const closeLogin = () => {
+  lightbox.style.display = "none";
+  loginContainer.style.display = "none";
+}
+const submitLogin = () => {
   
 }
 
-
-
+document.querySelector("#close-button").addEventListener("click",closeLogin);
 document.querySelector("#submit-button").addEventListener("click", () => {
-    alert("Ouch! Stop poking me!");
+    ;
   });
 
 
-
+const lightbox = document.querySelector("#lightbox")
 const login = document.querySelector("#open-login");
 const loginContainer = document.querySelector("#container-for-login");
 login.addEventListener("click",openLogin);
